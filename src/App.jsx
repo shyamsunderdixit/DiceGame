fcf050d9fde1a7cdf7f73e39abc02df071843b9e
Shyam Sunder Dixit <shyamsunderdixit32@gmail.com>
2025-01-14 15:23:29 +0530

    Created Project folder

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+function App() {
+  const Button = styled.button`
+  background-color: gray;
+  color: white;
+  padding: 12px;
+  font-weight: bold;
+  `
+
+  return (
+    <>
+      <Button>Hey buddy</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StartGame.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StartGame() {
+    return (
+    <div>StartGame</div>
+    )
+}
+
+export default StartGame
